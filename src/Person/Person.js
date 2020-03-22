52e6ea41ec4bb4976f0bb0742f0a05f8aa7dae53
Kuuga <ryuki.servaiv@gmail.com>
2020-03-22 23:57:11 +0700

    random math

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const person = () => {
-    return <p>Я человек!</p>
+    return <p>Я человек!, Мне {Math.floor(Math.random() * 30)} лет</p>
 };
 
 export default person;
