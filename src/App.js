827375b500c8c8cca6bb596868d5b80f7e19c7c7
Kuuga <ryuki.servaiv@gmail.com>
2020-03-23 00:32:15 +0700

    setState

--- src/App.js
+++ src/App.js
@@ -11,10 +11,21 @@ class App extends Component {
       {name:'Lee',age:12}
     ]
   }
+
+  swithNameHandler = () => {
+    this.setState({
+      persons:[
+        {name:'Uga xx',age:118},
+        {name:'Lee zz',age:12}
+      ]
+    })
+  }
+
   render(){
     return (
       <div className="App">
         <h1>привет</h1>
+        <button onClick={this.swithNameHandler}>Switch Button</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>Мой хобби, играю в игру</Person>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Мой хобби, сматю филмь</Person>
       </div>
